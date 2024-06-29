9219fa5d7cf02cb763bc6ed7f22e2a5453e33c78
Syed Ali Asar <sa.asar14@gmail.com>
2024-06-29 11:41:19 -0400

    setting up auth

--- app/auth.tsx
+++ app/auth.tsx
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import clientPromise from "@/lib/mongodb"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
+  adapter: MongoDBAdapter(clientPromise),
 })
